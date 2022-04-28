9ea6ea7dc882e55c550000ef468b0e147aba5149
comicstrip12345 <sabanalers@gmail.com>
2022-04-28 17:12:28 +0800

    Added Content for About Me Page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Profile from './Profile'
+import SkillsExp from './SkillsExp'
+import HobCert from './HobCert'
+import ContactUs from './ContactUs'
+import Footer from './Footer'
 
 const AboutMe = () => {
     return (
         <div>
             <Navbar/>
+            <Profile/>
+            <SkillsExp/>
+            <HobCert/>
+            <ContactUs/>
+            <Footer/>
         </div>
     )
 }
---
+++ src/components/SkillsExp.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import '../css/skillsexp.css'
+
+const SkillsExp = () => {
+    return (
+        <section className="skill-exp">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-5 col-md-12 col-lg-5 col-xl-5 skills">
+                        <div className="row">
+                            <div className="col-12 title">
+                                <h1>Skills</h1>
+                            </div>
+                            <div className="col-12 skill-title">
+                                <h1>Front-end</h1>
+                            </div>
+                            <div className="col-12 tools">
+                                <p>
+                                    <a href="/#">HTML5</a>
+                                    <a href="/#">CSS3</a>
+                                    <a href="/#" className="js">Javascript ES6</a>
+                                    <br/> <br/>
+                                    <a href="/#">Bootstrap 5</a>
+                                    <a href="/#" className="mcss">materializeCSS</a>
+                                </p>
+                                
+                            </div>
+                            <div className="col-12 skill-title des">
+                                <h1>Design</h1>
+                            </div>
+                            <div className="col-12 tools">
+                                <p>
+                                    <a href="/#" className="figma">Figma</a>
+                                </p>
+                            </div>
+                            <div className="col-12 skill-title ver">
+                                <h1>Version Control</h1>
+                            </div>
+                            <div className="col-12 tools">
+                                <p>
+                                    <a href="/#">GitHub</a>
+                                    <a href="/#" className="git">Git</a>
+                                </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-7 col-md-12 col-lg-7 col-xl-7 exp">
+                        <div className="row">
+                            <div className="col-12 title">
+                                <h1>Experiences</h1>
+                            </div>
+                            <div className="col-12 col-sm-5 col-xl-5 d-flex align-items-sm-center exp-date">
+                                <h1>Feb. 2022 - Present</h1>
+                            </div>
+                            <div className="col-12 col-sm-7 col-xl-7 exp-place kodego">
+                                <h1>KodeGo</h1> 
+                                <h1 className="exp-date">Feb. 2022 - Present</h1>
+                                <div className="row">
+                                    <div className="col-4 exp-place-content">
+                                        <p>Position:</p>
+                                        <p>Specialization:</p>
+                                        <p>Industry:</p>
+                                    </div>
+                                    <div className="col-8 exp-place-content">
+                                        <p>Trainee</p>
+                                        <p>Front-end Web Developer</p>
+                                        <p>Computer Systems Design and Related Services</p>
+                                    </div>
+                                    <div className="col-12 exp-place-content">
+                                        <p>- Incorporate the HTML Design into HTML file.</p>
+                                        <p>
+                                            - Design the HTML website according to the client’s <br/> &nbsp; specifications.
+                                        </p>
+                                        <p>
+                                            - Understanding the basic concepts of HTML, CSS, <br/> &nbsp; Javascript, and Bootstrap 5 for developing a website.
+                                        </p>
+                                        <p>
+                                            - Understanding the tools needed for Version Control <br/> &nbsp; like GitHub and Git.
+                                        </p>
+                                        <p>
+                                            - Knowing the useful tips needed to accomplish the <br/> &nbsp; website.
+                                        </p>
+                                        <p>
+                                            - Knowing the coding standards needed for <br/> &nbsp; debugging and reviewing the codes of website.
+                                        </p>
+                                        <p>- has created 2 websites. Click <a href="/#">here</a> to view.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-5 col-sm-5 col-xl-5 d-flex align-items-center exp-date">
+                                <h1>Dec. 2021 - Jan. 2022</h1>
+                            </div>
+                            <div className="col-7 col-sm-7 col-xl-7 exp-place">
+                                <h1>Swiss IT Academy</h1>
+                                <h1 className="exp-date">Dec. 2021 - Jan. 2022</h1>
+                                <div className="row">
+                                    <div className="col-4 exp-place-content">
+                                        <p>Position:</p>
+                                        <p>Specialization:</p>
+                                        <p>Industry:</p>
+                                    </div>
+                                    <div className="col-8 exp-place-content">
+                                        <p>Trainee</p>
+                                        <p>Front-end Web Developer</p>
+                                        <p>Computer Systems Design and Related Services</p>
+                                    </div>
+                                    <div className="col-12 exp-place-content">
+                                        <p>
+                                            - Understanding the basic concepts of HTML, CSS, <br/> &nbsp; Javascript, and materializeCSS for developing a <br/> &nbsp; website
+                                        </p>
+                                        <p>
+                                            - Knowing the useful tips needed to accomplish the <br/> &nbsp; website 
+                                        </p>
+                                        <p>
+                                            - Knowing how to deploy the website using Netlify
+                                        </p>
+                                        <p>- has created 1 website. Click <a href="/#">here</a> to view.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SkillsExp
