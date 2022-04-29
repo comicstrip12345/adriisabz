89abaa49eacf6b0b22086f55cb4de3da343bdb95
comicstrip12345 <sabanalers@gmail.com>
2022-04-29 13:21:16 +0800

    Added Contact Us Image

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -6,21 +6,26 @@ const ContactUs = () => {
         <section className="contact-us">
             <div className="container">
                 <div className="row">
-                    <div className="col-12 title">
-                        <h1>Keep in touch</h1>
+                    <div className='col-sm-5 col-lg-5 col-xl-6 content'>
+                        <div className="col-12 title">
+                            <h1>Keep in touch</h1>
+                        </div>
+                        <div className="col-12 col-xl-6 form">
+                            <form action="">
+                                <p>Name: </p>
+                                <input type="text" name="" id=""/>
+                                <p>Email Address:</p>
+                                <input type="text" name="" id=""/>
+                                <p>Subject:</p>
+                                <input type="text" name="" id=""/>
+                                <p>Message</p>
+                                <textarea name="" id="" ></textarea> <br/>
+                                <button className="send">Send</button>
+                            </form>
+                        </div>
                     </div>
-                    <div className="col-12 col-xl-6 form">
-                        <form action="">
-                            <p>Name: </p>
-                            <input type="text" name="" id=""/>
-                            <p>Email Address:</p>
-                            <input type="text" name="" id=""/>
-                            <p>Subject:</p>
-                            <input type="text" name="" id=""/>
-                            <p>Message</p>
-                            <textarea name="" id="" ></textarea> <br/>
-                            <button className="send">Send</button>
-                        </form>
+                    <div className='col-sm-7 col-lg-7 col-xl-6 image'>
+                        <img src={require('../images/contact-pic.png')} alt="contactPic"/>
                     </div>
                 </div>
             </div>
